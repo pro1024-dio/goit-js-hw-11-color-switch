cd57cf0b177e119d470063c55528e5ab2a541d66
Ivan Dernovyi <idernovoy@gmail.com>
2021-10-26 00:48:21 +0300

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -119,6 +119,40 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"index.js":[function(require,module,exports) {
 // import './sass/main.scss';
+const colors = ['#FFFFFF', '#2196F3', '#4CAF50', '#FF9800', '#009688', '#795548'];
+let idInterval;
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const colorSwitch = () => {
+  const body = document.querySelector('body');
+  const colorIndex = randomIntegerFromInterval(0, 5);
+  body.style.backgroundColor = colors[colorIndex];
+};
+
+const buttonStartHandler = event => {
+  event.target.disabled = true;
+  event.target.nextElementSibling.disabled = false;
+  idInterval = setInterval(colorSwitch, 1000);
+  event.target.removeEventListener("click", buttonStartHandler);
+  event.target.nextElementSibling.addEventListener("click", buttonStoptHandler);
+};
+
+const buttonStoptHandler = event => {
+  clearInterval(idInterval);
+  event.target.disabled = true;
+  event.target.removeEventListener("click", buttonStoptHandler);
+  event.target.previousElementSibling.disabled = false;
+  event.target.previousElementSibling.addEventListener("click", buttonStartHandler);
+};
+
+const buttonStart = document.querySelector('button[data-action="start"]');
+const buttonStop = document.querySelector('button[data-action="stop"]');
+buttonStop.disabled = true;
+buttonStart.addEventListener("click", buttonStartHandler); // console.log(buttonStart);
+// console.log(buttonStop);
 },{}],"../node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +181,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62026" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62094" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
